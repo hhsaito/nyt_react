a33aefbe58344d0d3d6efc3e2824b044eb6511a1
Harry Saito <hhsaito@gmail.com>
2017-03-13 22:32:14 -0500

    Heroku attempt

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static("./public"));
 // -------------------------------------------------
 
 // MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect("dbmongodb://heroku_0wc5csbw:password12345@ds129260.mlab.com:29260/heroku_0wc5csbw");
+mongoose.connect("dbmongodb://heroku_0wc5csbw:password12345@ds129260.mlab.com:29260/nytreact_db");
 var db = mongoose.connection;
 
 db.on("error", function(err) {
